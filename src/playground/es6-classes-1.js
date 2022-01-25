0b8762b68654bc59eeb3f28b3de2d287430b5922
rossanne <rossanneclamp@gmail.com>
2022-01-25 14:08:32 -0800

    component excercise

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -1,21 +1,20 @@
 class Person {
-  constructor(name, location = 'New York', age = 18) {
+  constructor(name, age = 18) {
     this.name = name; 
-    this.location = location;
     this.age = age; 
   }
   getGreetting(){
     return `Hi ${this.name} it's so nice to see you!`; 
   }
   getDescription(){
-    return `I hope it's not to dreary in ${this.location}. It's not a nice place to spend your ${this.age + 1} Birthday`;
+    return `It's not a nice place to spend your ${this.age + 1} Birthday`;
   }
 
 }
 
   class Student extends Person {
-    constructor(name, location, age, major) {
-      super(name, location, age);
+    constructor(name, age, major) {
+      super(name, age);
       this.major = major; 
     }
     hasMajor() {
@@ -24,17 +23,30 @@ class Person {
 
     getDescription() {
       let description = super.getDescription();
-
       if (this.hasMajor()) {
         description += ` Their Major is ${this.major}.`;
       }
       return description;
     }
-   
-
   }
 
-const me = new Student('Rossanne Clamp', 'Vancouver', 35, 'Visual Arts'); 
+  class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+      super(name, location, age); 
+      this.homeLocation = homeLocation; 
+    }
+    getGreetting() {
+      let greeting = super.getGreetting(); 
+      if (this.homeLocation) {
+        greeting += `hope your journey wasn't to long from ${this.homeLocation}.`;
+      }
+      return greeting;
+    }
+  }
 
 
-console.log(me.getDescription());
+const me = new Traveler('Rossannew Clamp', 36, 'North Van'); 
+
+const other = new Traveler();
+console.log(other.getGreetting());
+console.log(me.getGreetting());
